b65b6a8a9a67ff7ddefa30196745b58cb9bfe188
Mazharul Shameem <mazharulis.cs.07@gmail.com>
2022-11-09 02:09:23 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -7,6 +8,13 @@ const port = process.env.PORT || 5000;
 //middle wear
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.tejyxsb.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 app.get("/", (req, res) => {
   res.send("Zoom Wild Server Running");
