7d1564686f5cd9bbcd167d12e73b2f8361f41870
Mazharul Shameem <mazharulis.cs.07@gmail.com>
2022-11-09 22:43:15 +0600

    reviews post added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
 const app = express();
@@ -20,12 +20,19 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     const serviceCollection = client.db("zoomUser").collection("services");
+    const reviewCollection = client.db("zoomUser").collection("reviews");
 
     app.get("/services", async (req, res) => {
       const query = {};
       const cursor = serviceCollection.find(query);
       const services = await cursor.toArray();
       res.send(services);
+    });
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
     });
     app.get("/homeservices", async (req, res) => {
       const query = {};
@@ -34,7 +41,13 @@ async function run() {
       const services = await cursor.toArray();
       res.send(services);
     });
-    //add services
+    //add review
+    app.post("/reviews", async (req, res) => {
+      const review = req.body;
+      const result = reviewCollection.insertOne(review);
+      res.send(result);
+    });
+    // add services
     app.post("/services", async (req, res) => {
       const result = await serviceCollection.insertOne(req.body);
       console.log(result);
